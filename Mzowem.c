2a1bf395b50cbfecb28b8a2a7c9f5a79d4626649
aaronstanton <aaron.stanton@gmail.com>
2014-01-24 16:19:28 -0700

    got gazdag working

--- Mzowem.c
+++ Mzowem.c
@@ -43,6 +43,8 @@ void gazdag_2d_op(float **d, float **dmig,
                  float **c,
                  bool adj, bool verbose);
 void fk_op(sf_complex **m,float **d,int nw,int nk,int nt,int nx,bool adj);
+void k_op(sf_complex *m,sf_complex *d,int nk,int nx,bool adj);
+void f_op(sf_complex *m,float *d,int nw,int nt,bool adj);
 
 int main(int argc, char* argv[])
 {
@@ -251,7 +253,7 @@ void stolt_2d_op(float **d, float **dmig,
     for (iw=0;iw<nw;iw++){ 
       w = dw*iw;
       if ((w*w)/(vel*vel) - (k*k) > 0){ 
-        kz = sqrt((4*w*w)/(vel*vel) - (k*k));
+        kz = sqrt(4*(w*w)/(vel*vel) - (k*k));
         iwz = trunc(kz/dkz); 
        /* fprintf(stderr,"kz=%f, iwz=%d\n",kz,iwz);*/
       }
@@ -265,6 +267,111 @@ void stolt_2d_op(float **d, float **dmig,
   }
   if (adj) fk_op(mig,dmig,nwz,nk,nz,nmx,0); /* forward: mig to dmig */
   else     fk_op(m,d,nw,nk,nt,nmx,0); /* forward: m to d */
+  return;
+} 
+
+void gazdag_2d_op(float **d, float **dmig,
+                 int nt, float ot, float dt, 
+                 int nmx, float omx, float dmx,
+                 int nz, float oz, float dz,
+                 float **c,
+                 bool adj, bool verbose)
+/*< Gazdag zero offset wave equation depth migration operator >*/
+{
+
+/* 
+for 1: ndepth steps{
+  -d(f,k) = F{d(t,x)}
+  -apply the phase shift operator (depth extrapolation operator) for one depth step
+  -inverse fourier transform the data and put t=0 into dmig(idepth,:)
+} 
+*/
+  int iw,ik,iz,ix,nw,nk,padt,padx,ntfft;
+  float w,kz,k,vel,dw,dk;
+  sf_complex **m,*dmig_x,*dmig_k,**dmig_zk,czero,L;
+
+  __real__ czero = 0;
+  __imag__ czero = 0;
+  padt = 1;
+  padx = 1;
+  ntfft = padt*nt;
+  nw=ntfft/2+1;
+  nk = padx*nmx;
+  dk = 2*PI/nk/dmx;
+  dw = 2*PI/ntfft/dt;
+  m = sf_complexalloc2(nw,nk);
+  for (ik=0;ik<nk;ik++){
+    for (iw=0;iw<nw;iw++){
+      m[ik][iw] = czero;
+    }
+  }
+
+  if (adj){ /* extrapolate downward in depth */
+    fk_op(m,d,nw,nk,nt,nmx,1); /* d to m */
+    for (ix=0;ix<nmx;ix++) dmig[ix][0] = d[ix][0]; /* no need to extrapolate data at depth=0 */
+    for (iz=1;iz<nz;iz++){
+      fprintf(stderr,"extrapolating depth step %d/%d\n",iz+1,nz);
+      vel = c[0][iz]/2;
+      for (ik=0;ik<nk;ik++){
+        if (ik<nk/2) k = dk*ik;
+        else         k = -(dk*nk - dk*ik);
+        for (iw=0;iw<nw;iw++){ 
+          w = dw*iw;
+          if ((w*w)/(vel*vel) - (k*k)>0){
+            kz =-sqrt((w*w)/(vel*vel) - (k*k));
+            /* fprintf(stderr,"kz=%f\n",kz); */
+            __real__ L =  cos(kz*dz);
+            __imag__ L = -sin(kz*dz);
+            m[ik][iw] = m[ik][iw]*L;
+          }
+        }
+      }
+      fk_op(m,d,nw,nk,nt,nmx,0); /* forward: m to d. Now d is the recorded data at z=iz*dz, ready for extrapolation to next depth */ 
+      for (ix=0;ix<nmx;ix++) dmig[ix][iz] = d[ix][0]; /* apply the imaging condition */ 
+    }
+  }
+
+  else{ /* extrapolate upward in depth */
+    dmig_zk = sf_complexalloc2(nz,nk);
+    dmig_x = sf_complexalloc(nmx);
+    dmig_k = sf_complexalloc(nk);
+    for (iz=0;iz<nz;iz++){
+      for (ix=0;ix<nmx;ix++){ 
+        __real__ dmig_x[ix] = dmig[ix][iz];
+        __imag__ dmig_x[ix] = 0.0;
+      }
+      k_op(dmig_k,dmig_x,nk,nmx,1); /* dmig_x to dmig_k */
+      for (ik=0;ik<nk;ik++) dmig_zk[ik][iz] = dmig_k[ik];
+    }
+    for (ix=0;ix<nmx;ix++) d[ix][0] = dmig[ix][0]; /* no need to extrapolate data at time=0 */
+/*
+    for (iw=0;iw<nw;iw++){
+      for (ik=0;ik<nk;ik++){
+        m[ik][iw] = dmig_zk[ik][nz-1];
+      }
+    }
+*/
+    for (iz=nz-2;iz>0;iz--){
+      fprintf(stderr,"extrapolating depth step %d/%d\n",iz+1,nz);
+      vel = c[0][iz]/2;
+      for (ik=0;ik<nk;ik++){
+        if (ik<nk/2) k = dk*ik;
+        else         k = -(dk*nk - dk*ik);
+        for (iw=0;iw<nw;iw++){ 
+          w = dw*iw;
+          if ((w*w)/(vel*vel) - (k*k)>0){
+            kz =-sqrt((w*w)/(vel*vel) - (k*k));
+            /* fprintf(stderr,"kz=%f\n",kz); */
+            __real__ L =  cos(kz*dz);
+            __imag__ L =  sin(kz*dz);
+            m[ik][iw] = m[ik][iw]*L + dmig_zk[ik][iz];
+          }
+        }
+      }
+      fk_op(m,d,nw,nk,nt,nmx,0); /* forward: m to d. Now d is the recorded data at z=iz*dz, ready for extrapolation to next depth */ 
+    }
+  }
+
   return;
 } 
 
@@ -332,72 +439,73 @@ else{ /* model --> data */
 
 }
 
-void gazdag_2d_op(float **d, float **dmig,
-                 int nt, float ot, float dt, 
-                 int nmx, float omx, float dmx,
-                 int nz, float oz, float dz,
-                 float **c,
-                 bool adj, bool verbose)
-/*< Gazdag zero offset wave equation depth migration operator >*/
+void k_op(sf_complex *m,sf_complex *d,int nk,int nx,bool adj)
 {
-
-/* 
-for 1: ndepth steps{
-  -d(f,k) = F{d(t,x)}
-  -apply the phase shift operator (depth extrapolation operator) for one depth step
-  -inverse fourier transform the data and put t=0 into dmig(idepth,:)
-} 
-*/
-
-  int iw,ik,iz,ix,nw,nk,padt,padx,ntfft;
-  float w,kz,k,vel,dw,dk;
-  sf_complex **m,czero,L;
-  
+  sf_complex *a,*b,czero;
+  int *n,ix,ik;
+  fftwf_plan p1,p2;
   __real__ czero = 0;
   __imag__ czero = 0;
-  padt = 1;
-  padx = 1;
-  ntfft = padt*nt;
-  nw=ntfft/2+1;
-  nk = padx*nmx;
-  dk = 2*PI/nk/dmx;
-  dw = 2*PI/ntfft/dt;
-  m = sf_complexalloc2(nw,nk);
-  for (ik=0;ik<nk;ik++){
-    for (iw=0;iw<nw;iw++){
-      m[ik][iw] = czero;
-    }
+  a  = sf_complexalloc(nk);
+  b  = sf_complexalloc(nk);
+  n = sf_intalloc(1); n[0] = nk;
+  p1 = fftwf_plan_dft(1, n, (fftwf_complex*)a, (fftwf_complex*)a, FFTW_FORWARD, FFTW_ESTIMATE);
+  p2 = fftwf_plan_dft(1, n, (fftwf_complex*)b, (fftwf_complex*)b, FFTW_BACKWARD, FFTW_ESTIMATE);
+
+  if (adj){ /* x --> k */
+    for(ix=0 ;ix<nx;ix++) a[ix] = d[ix];
+    for(ix=nx;ix<nk;ix++) a[ix] = czero;
+    fftwf_execute(p1); 
+    for(ik=0 ;ik<nk;ik++) m[ik] = a[ik]; 
   }
-  /* extrapolate in depth */
-  fk_op(m,d,nw,nk,nt,nmx,1); /* adjoint: d to m */
-  for (ix=0;ix<nmx;ix++) dmig[ix][0] = d[ix][0]; /* no need to extrapolate data at depth=0 */
-  for (iz=1;iz<nz;iz++){
-    fprintf(stderr,"extrapolating depth step %d/%d\n",iz+1,nz);
-    vel = c[0][iz]/2;
-    for (ik=0;ik<nk;ik++){
-      if (ik<nk/2) k = dk*ik;
-      else         k = -(dk*nk - dk*ik);
-      for (iw=0;iw<nw;iw++){ 
-        w = dw*iw;
-        if ((w*w)/(vel*vel) - (k*k)>0){
-          kz =-sqrt((w*w)/(vel*vel) - (k*k));
-          /* fprintf(stderr,"kz=%f\n",kz); */
-          if (adj){ 
-              __real__ L =  cos(kz*dz);
-              __imag__ L = -sin(kz*dz);
-          }
-          else{ 
-              __real__ L =  cos(kz*dz);
-              __imag__ L =  sin(kz*dz);
-          }
-          m[ik][iw] = m[ik][iw]*L;
-        }
-      }
-    }
-    fk_op(m,d,nw,nk,nt,nmx,0); /* forward: m to d. Now d is the recorded data at z=iz*dz, ready for extrapolation to next depth */ 
-    for (ix=0;ix<nmx;ix++) dmig[ix][iz] = d[ix][0]; /* apply the imaging condition */ 
+  else{ /* k --> x */
+    for(ik=0; ik<nk;ik++) a[ik] = m[ik];
+    fftwf_execute(p2); 
+    for(ix=0; ix<nx;ix++) d[ix] = a[ix]/nk; 
+    for(ix=nx;ix<nk;ix++) d[ix] = czero;
   }
+  fftwf_destroy_plan(p1);fftwf_destroy_plan(p2);
+  fftwf_free(a);fftwf_free(b);
+
   return;
-} 
+}
+
+void f_op(sf_complex *m,float *d,int nw,int nt,bool adj)
+{
+  sf_complex *out1a,*in1b,czero;
+  float *in1a,*out1b;
+  int ntfft,it,iw;
+  fftwf_plan p1a,p1b;
+
+  ntfft = (nw-1)*2;
+  __real__ czero = 0;
+  __imag__ czero = 0;
+  out1a = sf_complexalloc(nw);
+  in1a = sf_floatalloc(ntfft);
+  p1a = fftwf_plan_dft_r2c_1d(ntfft, in1a, (fftwf_complex*)out1a, FFTW_ESTIMATE);
+  out1b = sf_floatalloc(ntfft);
+  in1b = sf_complexalloc(ntfft);
+  p1b = fftwf_plan_dft_c2r_1d(ntfft, (fftwf_complex*)in1b, out1b, FFTW_ESTIMATE);
+
+  if (adj){ /* data --> model */
+    for(it=0;it<nt;it++) in1a[it] = d[it];
+    for(it=nt;it<ntfft;it++) in1a[it] = 0;
+    fftwf_execute(p1a); 
+    for(iw=0;iw<nw;iw++) m[iw] = out1a[iw]; 
+    fftwf_destroy_plan(p1a);
+    fftwf_free(in1a); fftwf_free(out1a);
+  }
+
+  else{ /* model --> data */
+    for(iw=0;iw<nw;iw++) in1b[iw] = m[iw];
+    for(iw=nw;iw<ntfft;iw++) in1b[iw] = czero;
+    fftwf_execute(p1b); 
+    for(it=0;it<nt;it++) d[it] = out1b[it]/ntfft; 
+    fftwf_destroy_plan(p1b);
+    fftwf_free(in1b); fftwf_free(out1b);
+  }
+
+  return;
+}
 
 
